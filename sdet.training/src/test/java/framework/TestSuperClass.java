ebace54546041024eb9bf7b02cae47c374a9dfd0
dtaylor <dtaylor@TrainingVM>
2021-06-18 16:23:50 -0500

    Adds Protractor

--- sdet.training/src/test/java/framework/TestSuperClass.java
+++ sdet.training/src/test/java/framework/TestSuperClass.java
@@ -2,8 +2,12 @@ package framework;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
+
+import com.paulhammant.ngwebdriver.NgWebDriver;
+
 import framework.WebDriver.DriverManager;
 import framework.WebDriver.DriverManagerFactory;
 
@@ -29,6 +33,13 @@ public abstract class TestSuperClass {
 		DriverManager driverManager = DriverManagerFactory.getManager(settings.browserType);
 		driverManager.getDriver();
 		driver = driverManager.driver;
+		
+		JavascriptExecutor jsDriver = (JavascriptExecutor)driver;
+		NgWebDriver protractorDriver = new NgWebDriver(jsDriver);
+		
+		protractorDriver.waitForAngularRequestsToFinish();
+		protractorDriver.makeByAngularFactory();
+		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(settings.implicitTimeout, TimeUnit.MILLISECONDS);
 		driver.manage().timeouts().setScriptTimeout(settings.scriptTimeout, TimeUnit.MILLISECONDS);		
